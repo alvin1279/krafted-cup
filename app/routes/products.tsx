db521cfd5e3b08f908eeec4dc64e9c35b37ca86f
Alv-legion-lap <Alv@laptop.com>
2026-06-17 00:52:16 -0400

    Product filter section created

--- app/routes/products.tsx
+++ app/routes/products.tsx
@@ -1,27 +1,35 @@
 import Footer from "~/components/footer";
 import Header from "~/components/header";
 
-export default function Products(){
-    return(
-        <>
-        <Header />
-        <section className="menu-section min-h-[80dvh]">
-            <div className="menu-header min-h-1/5 max-h-1/4 bg-amber-600">
-                <h1 className=" text-8xl">Products</h1>
-                <div className="filter-section">
-                    <div className="search-bar">
-                        search
-                    </div>
-                    <div className="pre-filters">
-                        top
-                    </div>
-
-                </div>
-
+export default function Products() {
+  return (
+    <>
+      <Header />
+      <section className="menu-section min-h-[80dvh]">
+        <div className="menu-header min-h-1/5 max-h-1/4 bg-amber-600 pl-2 flex flex-col gap-1">
+          <h1 className="text-2xl sm:text-4xl md:text-6xl font-medium">
+            Products
+          </h1>
+          <div className="filter-section flex flex-col gap-2">
+            <div className="search-bar flex flex-row">
+              <input
+                className="bg-gray-100 rounded-full
+                         min-w-1xl w-2xs sm:w-2xl 
+                        pl-2.5  pr-2.5 
+                         overflow-y-hidden whitespace-nowrap overflow-x-scroll"
+                type="text"
+              />
+              <p className="m-1.5 bg-gray-600 pl-5 pr-5">Search</p>
             </div>
-
-        </section>
-        <Footer />
-        </>
-    )
+            <div className="pre-filters mb-1 ">
+              <p className="bg-brand-grad-b text-amber-700 pl-5 pr-5 w-fit rounded-full capitalize">
+                top
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
 }
